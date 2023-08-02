da4e9b910af53fb200f363425ba51aabff1f7f21
Sourcery AI <>
2023-08-02 15:20:45 +0000

    'Refactored by Sourcery'

--- submited/CLTZ.py
+++ submited/CLTZ.py
@@ -2,10 +2,7 @@ def collatz(n):
     i = 1
     while n!=1:
         i = i+1
-        if n%2==0:
-            n = n/2
-        else:
-            n = 3*n+1
+        n = n/2 if n%2==0 else 3*n+1
     return i;
 
 def main():
